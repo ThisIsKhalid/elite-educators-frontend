a792db2eff87c059cc91aa5f9d1fc99145b1047a
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-10-13 11:57:56 +0600

    created loading, error, not-found page

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+  return (
+    <div>
+        <h1>Something went wrong!</h1>
+    </div>
+  )
+}
+
+export default ErrorPage
---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+
+const Loading = () => {
+    return (
+        <div>
+            <h1>Loading...</h1>
+            <p>Please wait while we load the content.</p>
+        </div>
+    );
+};
+
+export default Loading;
