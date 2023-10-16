ff84b1e982b4d999635bd561f2bb061c6901c00b
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-10-17 01:31:44 +0600

    added private route to dashboard

--- src/app/(dashboard-layout)/layout.tsx
+++ src/app/(dashboard-layout)/layout.tsx
@@ -1,7 +1,28 @@
+"use client";
+
 import Sidebar from "@/components/ui/dashboard/Sidebar";
-import React from "react";
+import { isLoggedIn } from "@/services/auth.service";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
+  const userLoggedIn = isLoggedIn();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!userLoggedIn) {
+      router.push("/signin");
+    }
+
+    setIsLoading(true);
+  }, [router, isLoading, userLoggedIn]);
+
+  if (!isLoading) {
+    return <p>Redirecting...</p>;
+  }
+
   return (
     <section className="flex">
       <div className="">
