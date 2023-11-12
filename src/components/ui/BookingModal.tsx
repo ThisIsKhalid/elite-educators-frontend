9383772fe98c27b7a607b0c937073f9beb3f0580
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-11-12 23:45:27 +0600

    update booking card

--- src/components/ui/BookingModal.tsx
+++ src/components/ui/BookingModal.tsx
@@ -91,7 +91,7 @@ const BookingModal = ({ service }: any) => {
         </div>
         <div className="mt-2 text-center">
           <p className="text-cBlack font-bold font-mono uppercase text-xl mb-2">
-            Offered Service(per week) :
+            Offered Service(per week) <br /> Choose One :
           </p>
           <div className="flex flex-col items-center gap-2">
             {price?.map((priced: any) => (
@@ -100,7 +100,7 @@ const BookingModal = ({ service }: any) => {
                 className={` text-lg btn hover:bg-cOrange ${
                   selectedPrice === priced
                     ? "bg-cOrange text-gray-100"
-                    : "bg-cDeepBlue text-cyan-500"
+                    : "bg-gray-100 text-cDeepBlue border border-cDeepBlue"
                 }`}
                 key={priced._id}
               >
@@ -118,7 +118,7 @@ const BookingModal = ({ service }: any) => {
               id="startDate"
               value={formData.startDate}
               onChange={handleInputChange}
-              className="px-5 py-2 rounded-lg bg-cDeepBlue text-gray-100"
+              className="px-5 py-2 rounded-lg text-cDeepBlue border border-cDeepBlue"
             />
           </div>
           <div>
@@ -129,7 +129,7 @@ const BookingModal = ({ service }: any) => {
               id="endDate"
               value={formData.endDate}
               onChange={handleInputChange}
-              className="px-5 py-2 rounded-lg bg-cDeepBlue text-gray-100"
+              className="px-5 py-2 rounded-lg text-cDeepBlue border border-cDeepBlue"
             />
           </div>
         </div>
