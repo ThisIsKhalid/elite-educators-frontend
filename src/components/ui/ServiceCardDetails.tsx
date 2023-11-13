4bb858c3ac3d8c73d117f6d0f95633afb81e3664
ThisIsKhalid <iamkhalid.hs@gmail.com>
2023-11-13 21:22:32 +0600

    update service details

--- src/components/ui/ServiceCardDetails.tsx
+++ src/components/ui/ServiceCardDetails.tsx
@@ -4,8 +4,8 @@
 import { useGetReviewsByCourseIdQuery } from "@/redux/api/reviewsApi";
 import { useGetSingleServiceQuery } from "@/redux/api/serviceApi";
 import BookingModal from "./BookingModal";
-import ServiceReview from "./ServiceReview";
 import HashLoading from "./HashLoading";
+import ServiceReview from "./ServiceReview";
 
 const ServiceCardDetails = ({ id }: any) => {
   const { data, isLoading } = useGetSingleServiceQuery(id);
@@ -28,31 +28,8 @@ const ServiceCardDetails = ({ id }: any) => {
   } = data;
 
   return (
-    <div className="min-h-screen py-14 px-5">
+    <div className="min-h-screen py-14 md:px-24 px-5">
       <div className="grid lg:grid-cols-2 grid-cols-1 items-start gap-10">
-        <div className="flex flex-col gap-5">
-          <img
-            className=""
-            src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-            alt="Article"
-          />
-          <div className="">
-            <button
-              onClick={() => {
-                const dialog = document.getElementById(
-                  data?._id
-                ) as HTMLDialogElement;
-                if (dialog) {
-                  dialog.showModal();
-                }
-              }}
-              className="btn w-full hover:bg-cBlue bg-cOrange border-none text-gray-200"
-            >
-              Book Now
-            </button>
-            {data && <BookingModal service={data} />}
-          </div>
-        </div>
         <div>
           <div className="flex items-center gap-3">
             <h1 className="text-4xl font-bold uppercase">{subject}</h1>
@@ -114,6 +91,29 @@ const ServiceCardDetails = ({ id }: any) => {
                 </button>
               ))}
             </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5">
+          <img
+            className="rounded-lg"
+            src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="Article"
+          />
+          <div className="">
+            <button
+              onClick={() => {
+                const dialog = document.getElementById(
+                  data?._id
+                ) as HTMLDialogElement;
+                if (dialog) {
+                  dialog.showModal();
+                }
+              }}
+              className="btn w-full hover:bg-cBlue bg-cOrange border-none text-gray-200"
+            >
+              Book Now
+            </button>
+            {data && <BookingModal service={data} />}
           </div>
         </div>
       </div>
